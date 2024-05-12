ce41054c4644f4aeb5806b5e95c6e3aca58199dc
LucaYuji <160159812+LucaYuji@users.noreply.github.com>
2024-05-12 17:21:06 -0300

    Add files via upload

---
+++ types_of_variables.py
@@ -0,0 +1,12 @@
+# types of variables
+print(11 - 10 + 1980) #int
+print(1.5 + 1 + 0.5) #float
+print(True) #bool
+print(False) #bool
+print('Python') #str
+
+# int()
+# float()
+# bool()
+# str()
+
