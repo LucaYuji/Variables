59efce7c944a3f309060e575de70608fca170a1c
LucaYuji <160159812+LucaYuji@users.noreply.github.com>
2024-05-13 12:35:04 -0300

    Add files via upload

---
+++ print_input.py
@@ -0,0 +1,8 @@
+#input
+name = input('Inform your name: ')
+age = input('Inform your age: ')
+#print
+print(name, age)
+print(name, age, end=' ...\n')
+print(name, age, sep= ' a ')
+print(name, age, sep= ' a ', end=' ... \n')
