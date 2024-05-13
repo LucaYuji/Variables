52f8782e871e8a1822e21ce90b88745a5d1f5b70
LucaYuji <160159812+LucaYuji@users.noreply.github.com>
2024-05-13 12:14:59 -0300

    Add files via upload

---
+++ converting_types.py
@@ -0,0 +1,15 @@
+#converting types
+
+#int
+print(int(1.45))
+print(int('10'))
+#float
+print(float('10.10'))
+print(float('100'))
+#str
+value = 10
+value_str = str(value)
+print(type(value))
+print(type(value_str))
+# or
+print(type(str(10.10)))
