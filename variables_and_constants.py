61e41db1c95dea899349480b2d24ee8940cd8fb1
LucaYuji <160159812+LucaYuji@users.noreply.github.com>
2024-05-13 11:50:37 -0300

    Add files via upload

---
+++ variables_and_constants.py
@@ -0,0 +1,15 @@
+# variables and constants
+#Variables
+
+#name, age = 'Lucas', 20
+name = 'Lucas'
+age = 20
+name = 'Wilder'
+print(name, age)
+
+# Suggestive name
+# daily_withdrawal_limit = 1000
+
+#Contants
+BRAZILIAN_STATES = ['SP', 'RJ', 'SC', 'RS']
+print(BRAZILIAN_STATES)
